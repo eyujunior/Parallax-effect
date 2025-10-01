634323536b1d505700cf72399c5f3da408295140
eyujunior <eyujunior052@gmail.com>
2025-10-01 14:16:35 +0300

    initial cleanup and setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="font-display">
+      <h1 className="text-center text-9xl font-bold my-4">Parallax Website</h1>
+    </div>
+  );
+}
+
+export default App;
